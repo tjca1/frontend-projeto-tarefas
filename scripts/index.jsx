39cdc69e10dff898db5c040025402c1ac80050fa
Thiago Araujo <tjca1@hotmail.com>
2019-02-22 19:45:29 -0300

    ESQUELETO PROJETO REACT

---
+++ scripts/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './estilo'
+
+
+
+
+
+ReactDOM.render(
+    <h1>ESQUELETO PROJETO REACT</h1>,
+    document.getElementById('app')
+)
+
+
+
+
+
