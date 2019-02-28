838c5647ca8633e26cffcd369d1ee0ecd06c0b1e
Thiago Araujo <tjca1@hotmail.com>
2019-02-27 21:08:09 -0300

    Add atalho teclas no form

--- scripts/utils/reactUtils.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function filterChildren(children,props){
-    return React.Children.map(children,
-        child => React.cloneElement(child, {...props})
-       )
-}
-export{filterChildren}
--- src/template/iButao.jsx
+++ src/template/iButao.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import IfTest from '../ultils/ultil'
+import IfTest from '../ultils/ultilCmp'
 
 export default props => (
 
